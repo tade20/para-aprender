ef375083d8f64d466577be1f98d89294b56a654a
tade20 <tadeomachin@gmail.com>
2023-03-27 11:55:16 -0300

    part1D

--- part1C/src/main.jsx
+++ part1C/src/main.jsx
@@ -27,20 +27,55 @@ const Button = ({ handleClick, text }) => {
   )
 }
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = (props) => {
-  //const name = 'Peter'
-  //const age = 10
+  {/*
+  const name = 'Peter'
+  const age = 10
   const [counter, setCounter] = useState(0);
-  {/*setTimeout(
+  setTimeout(
     () => setCounter(counter + 1),
     10000
   )
-  console.log("Rendering...", counter)*/}
+  console.log("Rendering...", counter)
   const increaseByOne = () => setCounter(counter + 1)
   const decreaseByOne = () => setCounter(counter - 1)
   const resetCounter = () => setCounter(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }*/}
+  const [value, setValue] = useState(10)
+  const handleClick = () => {
+    console.log("clicked the button")
+    setValue(0)
+  }
   return (
     <div>
+      {/*
       <Display counter={counter} />
       <Button
         handleClick={increaseByOne}
@@ -54,10 +89,19 @@ const App = (props) => {
         handleClick={decreaseByOne}
         text='minus'
       />
-      {/*<h1>Greetings</h1>
-        <Hello name="Maya" age={26 + 10} />
-        <Hello name={name} age={age} />*/}
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
 
+      <History allClicks={allClicks} />*/}
+      <div>
+        {value}
+        <button onClick={handleClick}>Reset</button>
+      </div>
     </div>
   )
 }
