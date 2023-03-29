b801f7bb9ff5823adef8d1890b82ff583c1cc0f3
tade20 <tadeomachin@gmail.com>
2023-03-29 11:32:28 -0300

    exercises anecdotes

--- anecdotes/src/main.jsx
+++ anecdotes/src/main.jsx
@@ -1,12 +1,61 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Anecdote = ({ text, votesCount }) =>
+  <div>
+    <p>{text}</p>
+    <p>has {votesCount} votes</p>
+  </div>
+
+const Winner = ({ anecdotes, allVotes }) => {
+  const highestVoteCount = Math.max(...allVotes)
+  const winnerIndex = allVotes.indexOf(highestVoteCount)
+  const winner = anecdotes[winnerIndex]
+  if (highestVoteCount === 0) {
+    return (
+      <p>No votes yet</p>
+    )
+  }
 
   return (
     <div>
-      {props.anecdotes[selected]}
+      <p>{winner}</p>
+      <p>has {highestVoteCount} votes</p>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) =>
+  <button onClick={onClick}>
+    {text}
+  </button>
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0)
+  const [allVotes, setAllVotes] = useState(Array(6).fill(0))
+
+  const handleVoteClick = () => {
+    const newAllVotes = [...allVotes]
+    newAllVotes[selected] += 1
+    setAllVotes(newAllVotes)
+  }
+
+  const handleAnecdoteClick = () => {
+    const arrayIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(arrayIndex)
+  }
+
+  return (
+    <div>
+      <Header name="Anecdote of the day" />
+      <Anecdote text={anecdotes[selected]} votesCount={allVotes[selected]} />
+      <Button onClick={handleVoteClick} text="vote" />
+      <Button onClick={handleAnecdoteClick} text="Next anecdote" />
+
+      <Header name="Anecdote with most votes" />
+      <Winner anecdotes={anecdotes} allVotes={allVotes} />
     </div>
   )
 }
@@ -20,8 +69,9 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App anecdotes={anecdotes} />
   </React.StrictMode>,
 )
